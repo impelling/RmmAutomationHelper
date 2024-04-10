60922d9902ac5208c73dd5eeeb52c7b2d6b6929d
Chris Foulstone <chris@impelling.co.uk>
2024-04-10 14:48:42 +0100

    Install the modules required for build tools to run

--- Tools/Invoke-BuildAndImport.ps1
+++ Tools/Invoke-BuildAndImport.ps1
@@ -1,6 +1,16 @@
-Build-Module -VersionedOutputDirectory:$false
+$RequireModule = 'ModuleBuilder'
+
+if (-not (Get-Module -Name $RequireModule -ListAvailable)) {
+    Install-Module -Name $RequireModule -Scope CurrentUser -Force
+}
+
+if (-not (Get-Module -Name $RequireModule)) {
+    Import-Module -Name $RequireModule
+}
+
+Build-Module -VersionedOutputDirectory:$false -Verbose
 
 $ModuleName = Get-ChildItem -Path .\Output -Directory | Select-Object -First 1 -ExpandProperty Name
 
 Remove-Module $ModuleName -Force -ErrorAction SilentlyContinue
-Import-Module .\Output\$ModuleName\$ModuleName.psm1
+Import-Module .\Output\$ModuleName\$ModuleName.psm1 -Verbose
--- Tools/Update-Docs.ps1
+++ Tools/Update-Docs.ps1
@@ -2,6 +2,16 @@
 $ScriptPath = $MyInvocation.MyCommand.Path
 $ToolsDirectory = Split-Path $ScriptPath
 
+$RequireModule = 'PlatyPS'
+
+if (-not (Get-Module -Name $RequireModule -ListAvailable)) {
+    Install-Module -Name $RequireModule -Scope CurrentUser -Force
+}
+
+if (-not (Get-Module -Name $RequireModule)) {
+    Import-Module -Name $RequireModule
+}
+
 . (Join-Path $ToolsDirectory Invoke-BuildAndImport.ps1)
 
 Write-Output "Generating markdown help files"
